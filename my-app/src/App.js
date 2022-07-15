c2d32486ac7980827a809d5615995e27ffbc9e4a
jjos425 <jjos425@aucklanduni.ac.nz>
2022-07-15 23:09:23 +1200

    Using Create React App for website

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        My First React Page
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Advantages</li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
